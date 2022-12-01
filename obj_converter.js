0360408ace99524d8f7197146ee226a9022bf29b
iseeeva <107738269+iseeeva@users.noreply.github.com>
2022-12-01 05:49:48 +0300

    vertex normals added

--- obj_converter.js
+++ obj_converter.js
@@ -10,22 +10,24 @@ if (process.argv[2] === "--help" || process.argv[2] === undefined) {
 
 ////////////////////////////////////
 
-const objFile = new obj2js(fs.readFileSync(process.argv[2],"utf8"));
-const output = objFile.parse();
+let objFile = new obj2js(fs.readFileSync(process.argv[2],"utf8")); objFile = objFile.parse();
 
 let ibuf_build_name = `${(process.argv[2]).substr(0, (process.argv[2]).lastIndexOf('.'))}.pc_ibuf`,
     vbuf_build_name = `${(process.argv[2]).substr(0, (process.argv[2]).lastIndexOf('.'))}.pc_vbuf`;
+    // xml_build_name = `${(process.argv[2]).substr(0, (process.argv[2]).lastIndexOf('.'))}.pc_xml`; //xml
 
-fs.writeFileSync(vbuf_build_name,"");
-fs.writeFileSync(ibuf_build_name,"");
-
+fs.writeFileSync(vbuf_build_name,"");fs.writeFileSync(ibuf_build_name,"");
+let vbuf_stream = fs.createWriteStream(vbuf_build_name, {flags: "a",mode: 0744}),
+    ibuf_stream = fs.createWriteStream(ibuf_build_name, {flags: "a",mode: 0744});
+	
 let model_name = [];
-let vertex = [], uv = [], index = [];
+let vertex = [], vertex_normal = [], uv = [], index = [];
 let calc_vsize = 0, calc_vsize_total = 0, calc_vsize_array = [],
-    calc_usize = 0, calc_usize_total = 0, calc_usize_array = [];
+    calc_nsize = 0, calc_nsize_total = 0, calc_nsize_array = [],
+    calc_usize = 0, calc_usize_total = 0, calc_usize_array = [],
     calc_isize = 0, calc_isize_total = 0, calc_isize_array = [];
 
-output['models'].forEach((models) => {
+objFile['models'].forEach((models) => {
 	
 	model_name.push(models["name"]);
 	;(async () => {
@@ -38,6 +40,17 @@ output['models'].forEach((models) => {
 			// console.log("ver:",xyz.x,xyz.y,xyz.z)
 		})
 		calc_vsize_array.push(models["vertices"].length * 20);
+	})()
+
+	;(async () => {
+		models["vertexNormals"].forEach((xyz) => {
+			vertex_normal.push(float_to_hex(xyz.x));
+			vertex_normal.push(float_to_hex(xyz.y));
+			vertex_normal.push(float_to_hex(xyz.z));
+			vertex_normal.push(Buffer.from("00000000","hex"));
+			// console.log("vn:",xyz.x,xyz.y,xyz.z)
+		})
+		calc_nsize_array.push(models["vertexNormals"].length * 16);
 	})()
 	
 	;(async () => {
@@ -63,34 +76,36 @@ output['models'].forEach((models) => {
 
 ////////////////////////////////////
 
-let total_vertex = vertex.length/5, total_uv = uv.length/2, total_index = index.length/3;
+let total_vertex = vertex.length/5, total_vn = vertex_normal.length/4, total_uv = uv.length/2, total_index = index.length/3;
 
 calc_vsize_array.forEach((calc) => {calc_vsize_total+=calc});
+calc_nsize_array.forEach((calc) => {calc_nsize_total+=calc});
 calc_usize_array.forEach((calc) => {calc_usize_total+=calc});
 calc_isize_array.forEach((calc) => {calc_isize_total+=calc});
-
 calc_usize=calc_vsize_total;
+calc_nsize=calc_vsize_total+calc_usize_total;
+
 for(var i=0; i < calc_vsize_array.length; i++){
 	calc_vsize+=calc_vsize_array[i];
+	calc_nsize+=calc_nsize_array[i];
 	calc_usize+=calc_usize_array[i];
 	calc_isize+=calc_isize_array[i];
+	
 	console.log(
 	   `\r[MODEL] ${model_name[i]}
 	    \r[VERTEX] ${calc_vsize-calc_vsize_array[i]} [VERTEX_F] ${calc_vsize_array[i]/20}
+	    \r[VERTEX_NORMAL] ${calc_nsize-calc_nsize_array[i]} [VERTEX_F] ${calc_nsize_array[i]/16}
 	    \r[UV] ${calc_usize-calc_usize_array[i]} [UV_F] ${calc_usize_array[i]/8}
 	    \r[FACE] ${calc_isize-calc_isize_array[i]} [FACE_F] ${calc_isize_array[i]/6}
 		`
 	);
 }
 
-console.log("[VERTEX]", total_vertex, "[UV]", calc_vsize_total, total_uv, "[FACE]", total_index);
-
-let vbuf_stream = fs.createWriteStream(vbuf_build_name, {flags: "a",mode: 0744});
-let ibuf_stream = fs.createWriteStream(ibuf_build_name, {flags: "a",mode: 0744});
-
 vertex.forEach(async(vertex) => {await vbuf_stream.write(vertex);})
 uv.forEach(async(uv) => {await vbuf_stream.write(uv);})
+vertex_normal.forEach(async(vertex) => {await vbuf_stream.write(vertex);})
 index.forEach(async(index) => {await ibuf_stream.write(index);})
 vbuf_stream.end();ibuf_stream.end();
 
+console.log("[VERTEX]", total_vertex, "[VERTEX_NORMAL]", calc_vsize_total + calc_usize_total, total_vn, "[UV]", calc_vsize_total, total_uv, "[FACE]", total_index);
 console.log(vbuf_build_name,ibuf_build_name,"finished")
